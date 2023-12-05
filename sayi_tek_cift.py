7a3277e42e4ca32bbc5dac39120cf08929b761f4
nisa <103253915+ozkurnisa@users.noreply.github.com>
2023-12-05 23:51:20 +0300

    Add files via upload

---
+++ sayi_tek_cift.py
@@ -0,0 +1,11 @@
+#3. Girilen sayının tek mi çift mi olduğunu bulan program
+
+
+a=int (input("bir sayı giriniz:"))
+
+if a%2 == 0:
+    print("sayı çift")
+else:
+    print("sayı tek")
+
+
---
+++ sayi_toplama.py
@@ -0,0 +1,8 @@
+#1. Kullanıcıdan iki sayı alıp, bu sayıları toplayan bir program yazın
+
+a=int(input("1. sayıyı giriniz:"))
+b=int(input("2. sayıyı giriniz:"))
+
+c = a+b
+
+print(c)
